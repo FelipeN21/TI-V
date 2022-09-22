f9bb9439ae7c19515590d92778ca467be82214b2
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2022-09-22 20:22:03 -0300

    First Presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './dependencies/material.module';
+import { ArduinoComponent } from './components/arduino/arduino.component';
+import { RedeComponent } from './components/rede/rede.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-  ],
+  declarations: [AppComponent, ArduinoComponent, RedeComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
