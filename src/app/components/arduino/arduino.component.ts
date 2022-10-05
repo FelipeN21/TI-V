12fe2c8970e463916004b93bf1fdd94f65c42bdb
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2022-10-05 19:30:02 -0300

    C O M M I T DA RAIVA

--- src/app/components/arduino/arduino.component.ts
+++ src/app/components/arduino/arduino.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-arduino',
   templateUrl: './arduino.component.html',
-  styleUrls: ['./arduino.component.css']
+  styleUrls: ['./arduino.component.css'],
 })
 export class ArduinoComponent implements OnInit {
+  constructor() {}
+  CorLuz = true;
+  CorBomba = true;
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public AtivarLuz() {
+    if (this.CorLuz == true) this.CorLuz = false;
+    else this.CorLuz = true;
   }
-
+  public AtivarBomba() {
+    if (this.CorBomba == true) this.CorBomba = false;
+    else this.CorBomba = true;
+  }
 }
