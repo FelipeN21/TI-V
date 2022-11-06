a0c78d6afce542beaa04b9ca389a9c3c6f8171e4
FelipeN21 <felipenepomuceno21@hotmail.com>
2022-11-06 13:52:01 -0300

    Adicionados valores para recomendacao das plantas

--- src/app/plantas/plantas.ts
+++ src/app/plantas/plantas.ts
@@ -3,37 +3,37 @@ import { Planta } from './planta.model';
 export class Plantas {
   static MANJERICAO: Planta = {
     nome: 'Manjericão',
-    luminosidade: 0,
-    umidade: 0,
-    temperatura: 0,
+    luminosidade: 50,
+    umidade: 86.67,
+    temperatura: 20,
   };
 
   static SUCULENTA: Planta = {
     nome: 'Suculenta',
-    luminosidade: 0,
-    umidade: 0,
-    temperatura: 0,
+    luminosidade: 64,
+    umidade: 34.53,
+    temperatura: 32,
   };
 
   static CACTO: Planta = {
     nome: 'Cacto',
-    luminosidade: 0,
-    umidade: 0,
-    temperatura: 0,
+    luminosidade: 70,
+    umidade: 22.5,
+    temperatura: 30,
   };
 
   static ORQUIDEA: Planta = {
     nome: 'Orquídea',
-    luminosidade: 0,
-    umidade: 0,
-    temperatura: 0,
+    luminosidade: 120,
+    umidade: 55,
+    temperatura: 25,
   };
 
   static ALFACE: Planta = {
     nome: 'Alface',
-    luminosidade: 0,
-    umidade: 0,
-    temperatura: 0,
+    luminosidade: 60,
+    umidade: 70,
+    temperatura: 18,
   };
 
   static PLANTAS: Planta[] = [
